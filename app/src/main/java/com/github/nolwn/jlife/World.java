3be77373068489d7725b7baaeab4117aee9c8a5c
nolwn <nolan.hellyer@gmail.com>
2021-01-02 12:50:09 -0800

    Get cell within world

--- app/src/main/java/com/github/nolwn/jlife/World.java
+++ app/src/main/java/com/github/nolwn/jlife/World.java
@@ -1,29 +1,55 @@
 package com.github.nolwn.jlife;
 
 public class World {
-    public static final char DEAD = ' ';
-    public static final char ALIVE = '*';
+	public static final char DEAD = ' ';
+	public static final char ALIVE = '*';
 
-    private int width;
-    private int height;
+	private int width;
+	private int height;
+	private Cell[] cells;
 
-    public World(int width, int height) {
-        this.width = width;
-        this.height = height;
-    }
+	public World(int width, int height) {
+		this.width = width;
+		this.height = height;
+		this.cells = new Cell[width * height];
+		
+		for(int i = 0; i < width * height; i++) {
+			this.cells[i] = new Cell(false);
+		}
+	}
 
-    public String toString() {
-        String output = "";
+	public String toString() {
+		String output = "";
 
-        for(int i = 0; i < width * height; i++) {
-            output += DEAD;
+		for(int i = 0; i < width * height; i++) {
+			output += DEAD;
 
-            if ((i + 1) % width == 0) {
-                output += '\n';
-            }
+			if ((i + 1) % width == 0) {
+				output += '\n';
+			}
 
-        }
+		}
 
-        return output;
-    }
+		return output;
+	}
+	
+	public boolean getCell(int x, int y) {
+		if (!checkCoords(x, y)) {}
+
+		int len = (y - 1) * this.height + this.width;
+
+		return this.cells[len].isAlive();
+	}
+
+	private boolean checkCoords(int x, int y) {
+		if (x >= this.width) {
+			return false;
+
+		} else if (y >= this.height) {
+			return false;
+
+		} else {
+			return true;
+		}
+	}
 }
--- app/src/test/java/com/github/nolwn/jlife/WorldTest.java
+++ app/src/test/java/com/github/nolwn/jlife/WorldTest.java
@@ -4,17 +4,25 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class WorldTest {
-    @Test
-    public void CreateDeadWorld() {
-        World world = new World(4, 4);
-        String deadWorld = String.format(
-            "%1$c%1$c%1$c%1$c\n" + 
-            "%1$c%1$c%1$c%1$c\n" +
-            "%1$c%1$c%1$c%1$c\n" +
-            "%1$c%1$c%1$c%1$c\n", 
-            World.DEAD
-        );
+	@Test
+	public void createDeadWorld() {
+		World world = new World(4, 4);
+		String deadWorld = String.format(
+			"%1$c%1$c%1$c%1$c\n" + 
+			"%1$c%1$c%1$c%1$c\n" +
+			"%1$c%1$c%1$c%1$c\n" +
+			"%1$c%1$c%1$c%1$c\n", 
+			World.DEAD
+		);
 
-        assertEquals(deadWorld, world.toString(), "world should be a 4x4 dead world");
-    }
+		assertEquals(deadWorld, world.toString(), "world should be a 4x4 dead world");
+	}
+
+	@Test
+	public void getCellStatusWithinRange() {
+		World world = new World(4, 4);
+		boolean status = world.getCell(1, 2);
+
+		assertEquals(false, status);
+	}
 }
